278d8c06e387cdc993641e6ea1aa8e28393c311f
Piotrek Dziadkowiec <piotrek5994@gmail.com>
2024-02-29 14:18:49 +0100

    Add mongoDb settings

---
+++ Infrastracture/Settings/MongoDbSettings.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastracture.Settings
+{
+    public class MongoDbSettings
+    {
+        public string ConnectionUri { get; set; } = null!;
+        public string DatabaseName { get; set; } = null!;
+        public string CollectionName { get; set; } = null!;
+    }
+}
