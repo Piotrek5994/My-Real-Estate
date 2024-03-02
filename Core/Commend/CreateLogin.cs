eb95d47357ffd48ae654f6bb3f8a05f9b4ea9c4e
Piotrek Dziadkowiec <piotrek5994@gmail.com>
2024-03-02 19:03:47 +0100

    add required to login model

--- Core/Commend/CreateLogin.cs
+++ Core/Commend/CreateLogin.cs
@@ -1,14 +1,21 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace Core.Commend
 {
     public class CreateLogin
     {
+        [Required]
+        [JsonProperty(Required = Required.Always)]
         public string Email { get; set; }
+        [Required]
+        [JsonProperty(Required = Required.Always)]
         public string Password { get; set; }
     }
 }
