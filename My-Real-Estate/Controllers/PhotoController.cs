fd814bb8dd472ae5d2ad4dd347b829561ec32145
Piotrek Dziadkowiec <piotrek5994@gmail.com>
2024-03-18 09:05:57 +0100

    add new crod method to photo controller for PropertyPhoto

--- My-Real-Estate/Controllers/PhotoController.cs
+++ My-Real-Estate/Controllers/PhotoController.cs
@@ -42,16 +42,40 @@ public class PhotoController : Controller
     }
     [HttpPut]
     [Route("/Avatar")]
-    public async Task<IActionResult> UpdateAvatarPhoto(IFormFile formFile,string userId)
+    public async Task<IActionResult> UpdateAvatarPhoto(IFormFile formFile, string userId)
     {
-        string result = await _photoService.ChangePhoto(formFile,userId, "ChangeAvatar");
+        string result = await _photoService.ChangePhoto(formFile, userId, "ChangeAvatar");
         return Ok(new { AvatarId = result });
     }
     [HttpDelete]
     [Route("/Avatar")]
     public async Task<IActionResult> DeleteAvatarPhoto(string userId)
     {
-         await _photoService.DeleteAvatar(userId);
-        return Ok(new { Success = true});
+        await _photoService.DeleteAvatar(userId);
+        return Ok(new { Success = true });
+    }
+    [HttpGet]
+    [Route("/Photo")]
+    public async Task<IActionResult> GetPropertyPhoto()
+    {
+        return Ok(new { Success = true });
+    }
+    [HttpPost]
+    [Route("/Photo")]
+    public async Task<IActionResult> UploudPropertyPhoto()
+    {
+        return Ok(new { Success = true });
+    }
+    [HttpPut]
+    [Route("/Photo")]
+    public async Task<IActionResult> UpdatePropertyPhoto()
+    {
+        return Ok(new { Success = true });
+    }
+    [HttpDelete]
+    [Route("/Photo")]
+    public async Task<IActionResult> DeletePropertyPhoto()
+    {
+        return Ok(new { Success = true });
     }
 }
