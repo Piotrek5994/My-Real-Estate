18c74d96292843c911a8b8e317e931f4c224ec13
Piotrek Dziadkowiec <piotrek5994@gmail.com>
2024-03-21 13:44:53 +0100

    add create feature model dto

---
+++ Core/CommandDto/CreateFeatureDto.cs
@@ -0,0 +1,19 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.ComponentModel.DataAnnotations;
+
+namespace Core.CommandDto
+{
+    public class CreateFeatureDto
+    {
+        [Required]
+        [JsonProperty(Required = Required.Always)]
+        public string FeatureName { get; set; }
+    }
+}
--- Infrastracture/Repositories/FeaturesRepository.cs
+++ Infrastracture/Repositories/FeaturesRepository.cs
@@ -6,37 +6,22 @@ using Core.Model;
 using Infrastructure.Db;
 using Microsoft.Extensions.Logging;
 using MongoDB.Driver;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Infrastracture.Repositories;
 
 public class FeaturesRepository : IFeaturesRepository
 {
     private readonly MongoDbContext _context;
+    private readonly IPropertyRepository _propertyRepository;
     private readonly ILogger _log;
 
-    public FeaturesRepository(MongoDbContext context, ILogger<FeaturesRepository> log)
+    public FeaturesRepository(MongoDbContext context, ILogger<FeaturesRepository> log, IPropertyRepository propertyRepository)
     {
         _context = context;
         _log = log;
+        _propertyRepository = propertyRepository;
     }
     public async Task<List<Features>> GetFeatures(FeaturesFilter filter)
-    {
-        try
-        {
-            return null;  
-        }
-        catch(MongoException ex)
-        {
-            _log.LogError(ex, $"Error getting feature(s) type : Message - {ex.Message}");
-            return null;
-        }
-    }
-    public async Task<List<string>> CreateFeatures(CreateFeatures features,string propertyId)
     {
         try
         {
@@ -44,11 +29,33 @@ public class FeaturesRepository : IFeaturesRepository
         }
         catch (MongoException ex)
         {
-            _log.LogError(ex, $"Error create feature(s) type : Message - {ex.Message}");
+            _log.LogError(ex, $"Error getting feature(s) type : Message - {ex.Message}");
             return null;
         }
     }
-    public async Task<bool> UpdateFeature(UpdateFeature feature,string propertyId)
+    public async Task<List<string>> CreateFeatures(CreateFeatures features, string propertyId)
+    {
+        var resultMessages = new List<string>();
+        try
+        {
+
+            var chackProperty = await _propertyRepository.GetProperty(new PropertyFilter { Id = propertyId });
+            if (chackProperty == null || !chackProperty.Any())
+            {
+                _log.LogWarning("Warning: chosen property does not exist");
+                resultMessages.Add("The chosen property not exist");
+                return resultMessages;
+            }
+            return null;
+        }
+        catch (MongoException ex)
+        {
+            _log.LogError(ex, $"Error create feature(s) type : Message - {ex.Message}");
+            resultMessages.Add($"An error occurred while creating features: {ex.Message}");
+            return resultMessages;
+        }
+    }
+    public async Task<bool> UpdateFeature(UpdateFeature feature, string propertyId)
     {
         try
         {
